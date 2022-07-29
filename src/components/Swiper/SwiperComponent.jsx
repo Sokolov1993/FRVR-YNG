178ad0a76059e9be88ece07a6db57fd2cd7fac3c
Sokolov1993 <prokinoshow@gmail.com>
2022-07-29 22:22:36 +0300

    fixed slider and some styles

--- src/components/Swiper/SwiperComponent.jsx
+++ src/components/Swiper/SwiperComponent.jsx
@@ -1,4 +1,10 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+
+import Slide_1 from '../../assets/images/swiper/slide_1.jpg';
+import Slide_2 from '../../assets/images/swiper/slide_2.jpg';
+import Slide_4 from '../../assets/images/swiper/slide_4.jpg';
+import Slide_5 from '../../assets/images/swiper/slide_5.jpg';
+import Slide_6 from '../../assets/images/swiper/slide_6.jpg';
 
 import { Autoplay, EffectFade, Navigation, Pagination } from 'swiper';
 
@@ -19,7 +25,7 @@ const SwiperComponent = () => {
         <Swiper
           modules={[EffectFade, Navigation, Pagination, Autoplay]}
           autoplay={{
-            delay: 1000,
+            delay: 3500,
             disableOnInteraction: false,
           }}
           loop={true}
@@ -32,34 +38,35 @@ const SwiperComponent = () => {
           className="swiper"
         >
           <SwiperSlide>
-            <img
-              src={require('../../assets/images/swiper/slide_1.jpg')}
-              alt="some img"
-            />
+            <img src={Slide_1} alt="some img" />
+            Slide_1
+            <h2 className="title">
+              FOREVER LA - <span> New Collection </span>
+            </h2>
           </SwiperSlide>
           <SwiperSlide>
-            <img
-              src={require('../../assets/images/swiper/slide_2.jpg')}
-              alt="some img"
-            />
+            <img src={Slide_2} alt="some img" />
+            <h2 className="title">
+              FOREVER <span> YNG </span>
+            </h2>
           </SwiperSlide>
           <SwiperSlide>
-            <img
-              src={require('../../assets/images/swiper/slide_3.jpg')}
-              alt="some img"
-            />
+            <img src={Slide_4} alt="some img" />
+            <h2 className="title">
+              FOREVER LA<span>SY - Order Delivery!</span>
+            </h2>
           </SwiperSlide>
           <SwiperSlide>
-            <img
-              src={require('../../assets/images/swiper/slide_4.jpg')}
-              alt="some img"
-            />
+            <img src={Slide_5} alt="some img" />
+            <h2 className="title">
+              FOREVER LA<span>ST Day Discount</span>
+            </h2>
           </SwiperSlide>
           <SwiperSlide>
-            <img
-              src={require('../../assets/images/swiper/slide_5.jpg')}
-              alt="some img"
-            />
+            <img src={Slide_6} alt="some img" />
+            <h2 className="title">
+              FOREVER YNG<span> New Shop In Hollywood</span>
+            </h2>
           </SwiperSlide>
         </Swiper>
       </div>
